f2c8cc98be36a247775515f64be56daf5d72e20a
Carlos Eduardo Angulo Ustarez <carlos_angulo_az@hotmail.com>
2017-09-15 09:31:43 -0400

    SE aumento un proyecto NUnit Test

---
+++ NUnit-ProyectoCalculadora/IntroToNUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ProyectoCalculadora.Models;
+
+namespace NUnit_ProyectoCalculadora
+{
+  [TestFixture]
+  class IntroToNUnit
+  {
+    [Test]
+    public void TestResta() {
+      var TestCalculadora = new Calculadora();
+      int resultado = TestCalculadora.restar(4, 2);
+     
+      Assert.AreEqual(2, resultado);
+    }
+  }
+}
---
+++ ProyectoCalculadora/Models/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCalculadora.Models
+{
+  public class Calculadora
+  {
+    public int restar(int a, int b) {
+      return a-b;
+    }
+  }
+}
